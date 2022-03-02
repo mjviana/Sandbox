373bcaddca8f4b71fe660daa62bfed00e48c56d6
Mário Viana <mj.viana1993@hotmail.com>
2022-03-02 12:03:39 +0000

    Add factory functions

--- JavaScriptSandbox/objects.js
+++ JavaScriptSandbox/objects.js
@@ -1,14 +1,44 @@
 // Object-oriented programming (OOP)
 const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1
-  },
-  isVisible: true,
-  draw: function () {
-     console.log('draw');
-  }
-}; 
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('draw');
+    }
+};
 
 circle.draw(); // Method
+
+
+// Factory Functions
+function createCircle(radius, location, isVisible) {
+    return {
+        radius,
+        location,
+        draw() {
+            console.log('draw')
+        },
+        isVisible
+    };
+}
+
+const location1 = {
+    x: 1,
+    y: 1
+}
+const location2 = {
+    x: 2,
+    y: 2
+}
+
+const circle1 = createCircle(1, location1, true);
+console.log(`\ncircle #1: \n`, circle1);
+circle1.draw();
+
+const circle2 = createCircle(2, location2, false);
+console.log(`\ncircle #2: \n`, circle2);
+circle2.draw();
