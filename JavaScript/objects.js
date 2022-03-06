0274ebbeff19a8fbfafca4ba0db2b1de33b05d8a
Mário Viana <mj.viana1993@hotmail.com>
2022-03-06 15:33:28 +0000

    Add Dynamic nature of objects

--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -1,39 +1,38 @@
 // Object-oriented programming (OOP)
 const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    isVisible: true,
-    draw: function () {
-        console.log('draw');
-    }
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function () {
+    console.log("draw");
+  },
 };
 
 circle.draw(); // Method
 
-
 // Factory Functions
 function createCircle(radius, location, isVisible) {
-    return {
-        radius,
-        location,
-        draw() {
-            console.log('draw')
-        },
-        isVisible
-    };
+  return {
+    radius,
+    location,
+    draw() {
+      console.log("draw");
+    },
+    isVisible,
+  };
 }
 
 const location1 = {
-    x: 1,
-    y: 1
-}
+  x: 1,
+  y: 1,
+};
 const location2 = {
-    x: 2,
-    y: 2
-}
+  x: 2,
+  y: 2,
+};
 
 const circle1 = createCircle(1, location1, true);
 console.log(`\ncircle #1: \n`, circle1);
@@ -45,15 +44,29 @@ circle2.draw();
 
 // Constructor Functions
 function Circle(radius, location, isVisible) {
-    this.radius = radius;
-    this.location = location;
-    this.isVisible = isVisible;
-    this.draw = function () {
-        console.log('Draw');
-    }
+  this.radius = radius;
+  this.location = location;
+  this.isVisible = isVisible;
+  this.draw = function () {
+    console.log("Draw");
+  };
 }
 
-const circle3 = new Circle(3, location = { x: 3, y: 4 }, true);
+const circle3 = new Circle(3, (location = { x: 3, y: 4 }), true);
 
 console.log(`\n circle #3: \n`, circle3);
 circle3.draw();
+
+// Dynamic Nature of Objects
+const circle4 = {
+  radius: 1,
+};
+
+circle4.color = "yellow";
+circle4.draw = function () {};
+console.log(`\n circle whith added propety and function:`, circle4);
+
+delete circle4.color;
+delete circle4.draw;
+
+console.log(`\n circle whith removed propety and function: `, circle4);
