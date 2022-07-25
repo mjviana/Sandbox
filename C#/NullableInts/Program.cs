2976d2eae0ade694dcade8afc697e853063e74c0
Mário Viana <mj.viana1993@hotmail.com>
2022-07-25 11:45:53 +0100

    Add NullableInts project

---
+++ C#/NullableInts/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NullableInts;
+
+Console.WriteLine("Nullable ints!");
+
+var listA = new List<NullableUser>{
+    new NullableUser{
+        Id = null,
+        Name = "Alfredd"
+    },
+    new NullableUser{
+        Id = 1,
+        Name ="Bruce"
+    },
+    new NullableUser{
+        Name = "Clark"
+    }
+};
+
+List<User> listB = new List<User>();
+
+listA.ForEach(e =>
+{
+    var usr = new User
+    {
+        Id = e.Id ?? 0,
+        Name = e.Name
+    };
+    listB.Add(usr);
+}
+);
+
+System.Console.WriteLine("List A");
+listA.ForEach(e =>
+{
+    Console.WriteLine($"User -> {e.Id}, {e.Name}");
+});
+
+System.Console.WriteLine("List B");
+listB.ForEach(e =>
+{
+    Console.WriteLine($"User -> {e.Id}, {e.Name}");
+});
